773562e2ae211ef9886c04f7ff8a6eebad7ca75a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-03-26 23:54:48 +0200

    grvtr.js lib

---
+++ src/generateGravatar.js
@@ -0,0 +1,48 @@
+var	crypto = require('crypto'),
+	querystring = require('querystring');
+
+/*
+ * Trim from spaces helper
+ */
+function trim (str) {
+	return str.replace(/^\s*/, '').replace(/\s*$/, '');
+}
+
+/*
+ * Generates 'gravatar' url
+ *
+ * @param email {String} - user email that is using 'gravatar' service
+ * @param options {Object} - e.g. { size: 20, default: mm, rating: g, secure: false}
+ * @return {String} - url string
+ */
+function create (email, options) {
+	var clean = trim(email).toLowerCase(),
+		hash = crypto.createHash('md5').update(clean).digest('hex'),
+		options = options || {},
+		params = {},
+		baseUrl;
+
+	if (options.secure) {
+		baseUrl = 'https://gravatar.com/avatar/';
+	} else {
+		baseUrl = 'http://gravatar.com/avatar/';
+	}
+
+	if (options.size && !isNaN(options.size)) {
+		params.s = options.size;
+	}
+
+	if (options.default) {
+		params.d = options.default;
+	}
+
+	if (options.rating) {
+		params.r = options.rating;
+	}
+
+	params = '?' + querystring.stringify(params);
+
+	return baseUrl + hash + params;
+}
+
+exports.create = create;
---
+++ test/generateGravatar.spec.js
@@ -0,0 +1,42 @@
+describe('./src/generateGravatar.js', function () {
+	var grvtr = require('../src/generateGravatar.js'),
+		crypto = require('crypto');
+
+	describe('create() method', function () {
+		var email, hash;
+
+		beforeEach(function () {
+			email = 'dmitri.voronianski@gmail.com';
+			hash = crypto.createHash('md5').update(email).digest('hex');
+		});
+
+		it('should generate avatar url', function () {
+			expect(grvtr.create(email)).toBe('http://gravatar.com/avatar/' + hash + '?');
+		});
+
+		describe('when options passed', function () {
+
+			it('should add size option', function () {
+				expect(grvtr.create(email, { size: 20 })).toContain('?s=20');
+			});
+
+			it('should add default option', function () {
+				expect(grvtr.create(email, { default: 'mm' })).toContain('?d=mm');
+			});
+
+			it('should add rating option', function () {
+				expect(grvtr.create(email, { rating: 'g' })).toContain('?r=g');
+			});
+
+			it('should add secure option', function () {
+				expect(grvtr.create(email, { secure: true })).toContain('https');
+			});
+
+			it('should add several options', function () {
+				expect(grvtr.create(email, { size: 20, default: 'mm' })).toContain('?s=20&d=mm');
+			});
+
+		});
+	});
+
+});
